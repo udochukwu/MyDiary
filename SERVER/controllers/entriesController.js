99df0d200f1be596a681ba0f0bb7663539c72981
udochukwu <nnaji_udochukwu@yahoo.com>
2018-07-21 14:09:13 +0100

    setup application entry page,controller page,routes page and create a file which will contain dummydatabase

---
+++ SERVER/controllers/entriesController.js
@@ -0,0 +1,11 @@
+import { entries } from '../dummyDatabase/dummyDatabase';
+
+/**
+  * @class entriesController
+  * @description CRUD operations on Entries
+  */
+class EntriesController {
+
+}
+
+export default EntriesController;
---
+++ SERVER/routes/routes.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import EntriesController from '../controllers/entriesController';
+
+
+const router = express.Router();
+
+
+export default router;

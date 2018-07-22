f501fc45d68787afcdb8059b1385dd78e896fcb9
udochukwu <nnaji_udochukwu@yahoo.com>
2018-07-22 19:59:28 +0100

    create a class which handles request from from the EndPoint GET/entries/<entryId>

--- SERVER/routes/routes.js
+++ SERVER/routes/routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import EntriesController from '../controllers/entriesController';
 
-const { fetchAllEntries } = EntriesController;
+const { fetchAllEntries, fetchEntryById } = EntriesController;
 
 
 const router = express.Router();
 router.get('/entries', fetchAllEntries);
+router.get('/entries/:entryId', fetchEntryById);
 
 
 export default router;

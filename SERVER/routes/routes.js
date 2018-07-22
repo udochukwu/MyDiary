4e1e8bf13a7d27bd235ca201a739631ae6fe27c2
udochukwu <nnaji_udochukwu@yahoo.com>
2018-07-22 21:47:03 +0100

    Modidfy the entries controler(create a class which updates diary entries)

--- SERVER/routes/routes.js
+++ SERVER/routes/routes.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import EntriesController from '../controllers/entriesController';
 
-const { fetchAllEntries, fetchEntryById, createEntry } = EntriesController;
+const {
+  fetchAllEntries, fetchEntryById, createEntry, updateEntry
+} = EntriesController;
 
 
 const router = express.Router();
 router.get('/entries', fetchAllEntries);
 router.get('/entries/:entryId', fetchEntryById);
 router.post('/entries', createEntry);
+router.put('/entries/:entryId', updateEntry);
 
 
 export default router;

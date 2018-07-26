22f4bf8ab6f7106ab308f1546fbf648870402c5a
udochukwu <nnaji_udochukwu@yahoo.com>
2018-07-26 23:08:52 +0100

    modify the routes file, create a route which handles post request to the api endpoint auth/signup

--- SERVER/routes/routes.js
+++ SERVER/routes/routes.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import EntriesController from '../controllers/entriesController';
+import UsersController from '../controllers/usersController';
 
 const {
   fetchAllEntries, fetchEntryById, createEntry, updateEntry
 } = EntriesController;
-
+const { registerUser } = UsersController;
 
 const router = express.Router();
+router.post('/auth/signup', registerUser);
 router.get('/entries', fetchAllEntries);
 router.get('/entries/:entryId', fetchEntryById);
 router.post('/entries', createEntry);

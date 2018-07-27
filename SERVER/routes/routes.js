c27fe7bc79f25e47413e06a6008a81157f4c68f6
udochukwu <nnaji_udochukwu@yahoo.com>
2018-07-27 08:11:30 +0100

    Create routes for the API endpoint "/auth/login"

--- SERVER/routes/routes.js
+++ SERVER/routes/routes.js
@@ -3,13 +3,14 @@ import EntriesController from '../controllers/entriesController';
 import UsersController from '../controllers/usersController';
 
 const {
-  fetchAllEntries, fetchEntryById, createEntry, updateEntry
+  fetchUserEntries, fetchAllEntries, fetchEntryById, createEntry, updateEntry
 } = EntriesController;
 const { registerUser, login } = UsersController;
 
 const router = express.Router();
 router.post('/auth/signup', registerUser);
 router.post('/auth/login', login);
+router.get('/entries/user/:userId', fetchUserEntries );
 router.get('/entries', fetchAllEntries);
 router.get('/entries/:entryId', fetchEntryById);
 router.post('/entries', createEntry);

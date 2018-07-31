eef52778e8a24ed161df98d4e2ae6555947a3cd8
udochukwu <nnaji_udochukwu@yahoo.com>
2018-07-31 08:53:43 +0100

    add a function which authenticates token

--- SERVER/middlewares/authenticator.js
+++ SERVER/middlewares/authenticator.js
@@ -5,5 +5,35 @@ export default {
   generateToken(user) {
     const token = jwt.sign({ user }, process.env.JWTKEY, { expiresIn: '1200s' });
     return token;
+  },
+
+  checkToken(req, res, next) {
+    const token = req.body.token || req.query.token || req.headers['x-access-token'];
+    // const token = req.headers.authorization || req.body.token || req.query.token;
+    if (!token) {
+      res.status(403)
+        .json({
+          success: true,
+          message: 'Missing Token'
+        });
+    } else {
+      jwt.verify(token, process.env.JWTKEY, (err, user) => {
+        if (err) {
+          if (err.message.includes('signature')) {
+            res.status(403)
+              .json({
+                message: 'Invalid token supplied',
+              });
+          } else {
+            res.status(403)
+              .json({
+                message: err,
+              });
+          }
+        }
+        req.user = user;
+        return next();
+      });
+    }
   }
 };

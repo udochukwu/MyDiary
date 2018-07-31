a2a0d110f4e27171d97e6d60aed3cb715b503c28
udochukwu <nnaji_udochukwu@yahoo.com>
2018-07-31 12:52:50 +0100

    modify createTables file

--- SERVER/db/createTables.js
+++ SERVER/db/createTables.js
@@ -1,19 +1,31 @@
 
 import db from './dbConfig';
 
+// const dropTable = 'DROP TABLE IF EXISTS users CASCADE;';
+const createUserTable = `CREATE TABLE users(
+    userId SERIAL PRIMARY KEY,
+    email VARCHAR(40) UNIQUE NOT NULL,
+    password VARCHAR(40) NOT NULL,
+    registerdOn TIMESTAMP)`;
 
-db.query('CREATE TABLE users(userId SERIAL PRIMARY KEY, email VARCHAR(40) UNIQUE NOT NULL, password VARCHAR(40) NOT NULL,registerdOn TIMESTAMP);', (err) => {
-  if (err) {
-    console.log(`Could not create  table users ${err}`);
-  } else {
-    console.log('Users Table was successfully Created');
-  }
-});
+const createEntriesTable = `CREATE TABLE entries(
+      entryId SERIAL,
+      userId INT NOT NULL 
+      REFERENCES users(userId) ON DELETE CASCADE,
+      entryTitle VARCHAR(40) NOT NULL,
+      entryContent TEXT NOT NULL,
+      dateTime TIMESTAMP NOT NULL,
+      PRIMARY KEY (userId,entryId))`;
 
-db.query('CREATE TABLE entries(entryId SERIAL, userId INT NOT NULL REFERENCES users(userId) ON DELETE CASCADE, entryTitle VARCHAR(40) NOT NULL, entryContent TEXT NOT NULL, dateTime TIMESTAMP NOT NULL, PRIMARY KEY (userId,entryId));', (err) => {
+db.query(createUserTable, (err) => {
   if (err) {
-    console.log(`Could not create  table entris  ${err}`);
-  } else {
-    console.log('Entries Table was successfully created');
+    console.log(`Could not create table ${err}`);
   }
+  db.query(createEntriesTable, (err) => {
+    if (err) {
+      console.log(`Could not create  entries table  ${err}`);
+    } else {
+      console.log('Tables was successfully created');
+    }
+  });
 });

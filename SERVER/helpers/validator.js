6640e7596ebeede207e99a597dd10abc382da35f
udochukwu <nnaji_udochukwu@yahoo.com>
2018-08-01 12:44:54 +0100

    update the validator file

--- SERVER/helpers/validator.js
+++ SERVER/helpers/validator.js
@@ -2,20 +2,26 @@ import validator from 'validator';
 
 export default {
   regValidation(req, res, next) {
-    const errors = [];
+    console.log(req.body);    
+    const errors = {};
     const { email, password, confirmPassword } = req.body;
     if (!validator.isEmail(email)) {
-      errors.email = 'invalid email';
+      errors['email'] = 'invalid email';
     }
     if (validator.isEmpty(password)) {
-      errors.password = 'Password Empty';
+      errors['password'] = 'Password Empty';
     }
     if (password !== confirmPassword) {
-      errors.confirmPassword = 'Password does not match Empty';
+      errors['confirmPassword'] = 'Password does not match';
     }
-    if (errors === undefined || errors.length === 0) {
+
+    if ( errors.length === 0) {
+    console.log('no errors');  
       next();
     } else {
+    console.log(`length  equals ${errors.length}`);              
+      
+    console.log(errors);            
       res.status(406).json({ message: 'Please check your inputs', errors });
     }
   }

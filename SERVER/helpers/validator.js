c579012ed1c5981bcee274dfc8d743b03ffe3654
udochukwu <nnaji_udochukwu@yahoo.com>
2018-08-01 10:23:33 +0100

    delete the validator files and create a single validator file

---
+++ SERVER/helpers/validator.js
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
+
+export default {
+checkToken(req, res, next) {
+    const token = req.body.token || req.query.token || req.headers['x-access-token'];
+    if (!token) {
+      res.status(403)
+        .json({
+          success: false,
+          message: 'Missing Token'
+        });
+    } else {
+      jwt.verify(token, process.env.JWTKEY, (err, user) => {
+        if (err) {
+          if (err.message.includes('signature')) {
+            res.status(403)
+              .json({
+                message: 'Invalid token supplied',
+              });
+          } else {
+            res.status(403)
+              .json({
+                message: err,
+              });
+          }
+        }
+        // req.user = user;
+        next();
+      });
+    }
+  }
+};

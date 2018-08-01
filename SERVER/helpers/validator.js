49f14707589d78c52dc2b86525022a05fbda0fbd
udochukwu <nnaji_udochukwu@yahoo.com>
2018-08-01 12:02:56 +0100

    modify the regvalidation  function

--- SERVER/helpers/validator.js
+++ SERVER/helpers/validator.js
@@ -1,9 +1,22 @@
+import validator from 'validator';
 
 export default {
   regValidation(req, res, next) {
-    const{email, password, confirm_password } = req.body;
-
-    
-    next();
+    const errors = [];
+    const { email, password, confirmPassword } = req.body;
+    if (!validator.isEmail(email)) {
+      errors.email = 'invalid email';
+    }
+    if (validator.isEmpty(password)) {
+      errors.password = 'Password Empty';
+    }
+    if (password !== confirmPassword) {
+      errors.confirmPassword = 'Password does not match Empty';
+    }
+    if (errors === undefined || errors.length === 0) {
+      next();
+    } else {
+      res.status(406).json({ message: 'Please check your inputs', errors });
+    }
   }
 };

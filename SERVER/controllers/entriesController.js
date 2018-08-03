07cb63d4c23bae2c18d72b9dc79f0823a880c98f
udochukwu <nnaji_udochukwu@yahoo.com>
2018-08-03 09:38:02 +0100

    modify css file

--- SERVER/controllers/entriesController.js
+++ SERVER/controllers/entriesController.js
@@ -14,7 +14,7 @@ class EntriesController {
       }
       const { rows, rowCount } = dbRes;
       if (rowCount === 0) {
-        return res.status(200).json({ success: false, message: 'No Entries for user', rows });
+        return res.status(200).json({ success: true, message: 'No Entries for user', rows });
       }
       return res.status(200).json({ success: true, message: 'Entries successfully Loaded', entries: rows });
     });
@@ -54,7 +54,7 @@ class EntriesController {
     const userId = req.user.userid;
     const { entryId } = req.params;
 
-    db.query(updateEntry, [entryTitle, entryContent, entryId, userId], (err, dbRes) => {
+    db.query(updateEntry, [entryTitle, entryContent, entryId, userId], (err) => {
       if (err) {
         return res.json({ sucess: false, message: 'Could not update entry', err });
       }
@@ -65,7 +65,7 @@ class EntriesController {
   static deleteEntry(req, res) {
     const userId = req.user.userid;
     const { entryId } = req.params;
-    db.query(deleteEntry, [entryId, userId], (err, dbRes) => {
+    db.query(deleteEntry, [entryId, userId], (err) => {
       if (err) {
         return res.json({ sucess: false, message: 'Could not Delete entry', err });
       }

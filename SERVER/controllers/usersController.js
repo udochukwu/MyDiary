0d247402b5f2f0a04a6e3b04f74a45ff82f7f9ac
udochukwu <nnaji_udochukwu@yahoo.com>
2018-08-15 11:04:16 +0100

    modify the user controller

--- SERVER/controllers/usersController.js
+++ SERVER/controllers/usersController.js
@@ -19,11 +19,10 @@ class UsersController {
       if (rowCount !== 1) {
         return res.status(401).json({ success: false, message: 'Incorrect Email or password' });
       }
-      const user = rows[0];
       const { userid } = rows[0];
       const token = generateToken({ email, userid });
       res.status(200).json({
-        success: true, message: 'User successfully Logged In', user, token
+        success: true, message: 'User successfully Logged In', token
       });
     });
   }
@@ -38,12 +37,11 @@ class UsersController {
         return res.status(409).json({ success: false, message: 'Email Address Already Exists on our database' });
       }
       const { rows } = dbRes;
-      const user = rows[0];
       const { userid } = rows[0];
 
       const token = generateToken({ email, userid });
       return res.status(201).json({
-        success: true, message: 'User successfully Registerd', user, token
+        success: true, message: 'User successfully Registerd', token
       });
     });
   }

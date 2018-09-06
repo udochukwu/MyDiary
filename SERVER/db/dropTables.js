7eb45c36a6e8b851ccdd6feca4076f1af857974f
Nelson Nnaji <nelsonnnaji@MacBook-Pro-4.local>
2018-09-06 15:24:48 +0100

    modify he drop`table.js file

--- SERVER/db/dropTables.js
+++ SERVER/db/dropTables.js
@@ -8,12 +8,14 @@ const dropEntriesTable = 'DROP TABLE IF EXISTS entries CASCADE;';
 db.query(dropUsersTable, (err) => {
   if (err) {
     console.log(`Could not dropTable Users ${err}`);
+  }else{
+    console.log('Users table  was successfully Droped');
   }
-  console.log('Users table  was successfully Droped');
   db.query(dropEntriesTable, (err) => {
     if (err) {
       console.log(`Could not dropTableEntries ${err}`);
+    }else{
+      console.log('Table Entries was successfully Droped');
     }
-    console.log('Table Entries was successfully Droped');
   });
 });
